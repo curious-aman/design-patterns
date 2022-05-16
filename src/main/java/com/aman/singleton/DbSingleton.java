b1a1d91db783f0b83c7a3b697df86af685637024
Amanpreet Rana <aman1771988@gmail.com>
2022-05-16 22:41:20 +0100

    Added changes to make singleton thread safe and throw exception when using reflection to create object

--- src/main/java/com/aman/singleton/DbSingleton.java
+++ src/main/java/com/aman/singleton/DbSingleton.java
@@ -1,12 +1,48 @@
 package com.aman.singleton;
 
+import org.apache.derby.client.am.SqlException;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DbSingleton {
-    private static DbSingleton instance = new DbSingleton();
+  private static volatile DbSingleton instance = null;
+  private static volatile Connection conn = null;
 
-    private DbSingleton() {
+  private DbSingleton() {
+    if (conn != null) {
+      throw new RuntimeException("Use getInstance() method to create");
     }
+    if (instance != null) {
+      throw new RuntimeException("Use getInstance() method to create");
+    }
+  }
 
-    public static DbSingleton getInstance() {
-        return instance;
+  public static DbSingleton getInstance() {
+    if (instance == null) {
+      synchronized (DbSingleton.class) {
+        if (instance == null) {
+          instance = new DbSingleton();
+        }
+      }
     }
+    return instance;
+  }
+
+  public Connection getConnection() {
+    if (conn == null) {
+      synchronized (DbSingleton.class) {
+        if (conn == null) {
+          try {
+            String dbUrl = "jdbc:derby:memory:codejava/webdb;create=true";
+            conn = DriverManager.getConnection(dbUrl);
+          } catch (SQLException e) {
+            e.printStackTrace();
+          }
+        }
+      }
+    }
+    return conn;
+  }
 }
--- src/main/java/com/aman/singleton/DbSingletonExample.java
+++ src/main/java/com/aman/singleton/DbSingletonExample.java
@@ -1,10 +1,29 @@
 package com.aman.singleton;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DbSingletonExample {
   public static void main(String[] args) {
+    long timeBefore = 0;
+    long timeAfter = 0;
     DbSingleton dbSingleton = DbSingleton.getInstance();
-    System.out.println(dbSingleton);
-    DbSingleton anotherInstance = DbSingleton.getInstance();
-    System.out.println(anotherInstance);
+    timeBefore = System.currentTimeMillis();
+    Connection conn = dbSingleton.getConnection();
+    timeAfter = System.currentTimeMillis();
+    System.out.println(timeAfter - timeBefore);
+    try (Statement sta = conn.createStatement()) {
+      int count =
+          sta.executeUpdate(
+              "CREATE TABLE Address(ID INT, StreetName VARCHAR(20), City VARCHAR(20))");
+      System.out.println("Table Created.");
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    timeBefore = System.currentTimeMillis();
+    conn = dbSingleton.getConnection();
+    timeAfter = System.currentTimeMillis();
+    System.out.println(timeAfter - timeBefore);
   }
 }
