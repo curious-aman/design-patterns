9e27da5f4b8a0f5f98f5ffc96de28e564b5b6202
Amanpreet Rana <aman1771988@gmail.com>
2022-05-22 10:29:02 +0100

    Added changes for Abstract factory design pattern

---
+++ src/main/java/com/aman/creational/abstractfactory/AmexFactory.java
@@ -0,0 +1,2 @@
+package com.aman.creational.abstractfactory;public class AmexFactory {
+}
---
+++ src/main/java/com/aman/creational/abstractfactory/CardType.java
@@ -0,0 +1,5 @@
+package com.aman.creational.abstractfactory;
+
+public enum CardTyoe {
+    GOLD,PLATINUM;
+}
